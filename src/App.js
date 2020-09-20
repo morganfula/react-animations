f31796b53c4579db68df254c60530047f6846f43
morganfula <morgan.fula@me.com>
2020-09-20 16:08:22 +0200

    done

--- src/App.js
+++ src/App.js
@@ -9,29 +9,47 @@ import imgBoy from "./images/boy.webp";
 function App() {
   let app = useRef(null);
   let images = useRef(null);
-
-  let tl = new TimelineLite();
+  let content = useRef(null);
+  let tl = new TimelineLite({ delay: .8 });
 
   useEffect(() => {
-    // Images vars
+    // Images Vars
     const girlImage = images.firstElementChild;
     const boyImage = images.lastElementChild;
 
+    // Content Vars
+    const headLineFirst = content.children[0].children[0];
+    const headLineSecond = headLineFirst.nextSibling;
+    const headLineThird = headLineSecond.nextSibling;
+    const contentP = content.children[1];
+    const contentButton = content.children[2];
 
+    //Removing Initial Flash
     TweenMax.to(app, 0, { css: { visibility: "visible" } });
 
-    tl.from(girlImage, 1.2, { y: 1280, ease: Power3.easeOut })
+    // Images Animation
+    tl.from(girlImage, 1.2, { y: 1280, ease: Power3.easeOut }, 'Start')
       .from(girlImage.firstElementChild, 2, { scale: 1.6, ease: Power3.easeOut }, .2)
       .from(boyImage, 1.2, { y: 1280, ease: Power3.easeOut }, .2)
       .from(boyImage.lastElementChild, 2, { scale: 1.6, ease: Power3.easeOut }, .2)
-  });
+
+    // Content Animation
+    tl.staggerFrom([headLineFirst.children, headLineSecond.children, headLineThird.children], 1, {
+      y: 44,
+      ease: Power3.easeInOut,
+      delay: .8
+    }, .15, 'Start')
+      .from(contentP, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.4)
+      .from(contentButton, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.6)
+
+  }, [tl]);
 
   return (
     <div className='hero' ref={el => app = el}>
       <div className='container'>
         <div className='hero-inner'>
           <div className='hero-content'>
-            <div className='hero-content-inner'>
+            <div className='hero-content-inner' ref={el => content = el}>
               <h1>
                 <div className='hero-content-line'>
                   <div className='hero-content-line-inner'>
