4d47b5b892b1d166d5e3edead5dc0f5620affcd5
morganfula <morgan.fula@me.com>
2020-09-20 15:45:58 +0200

    img animation done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
-
-import arrow from "./images/arrow-right.svg";
+import React, { useRef, useEffect } from "react";
+import { TweenMax, TimelineLite, Power3 } from "gsap";
 import "./App.scss";
 
+import arrow from "./images/arrow-right.svg";
 import imgGirl from "./images/girl.webp";
 import imgBoy from "./images/boy.webp";
 
 function App() {
+  let app = useRef(null);
+  let images = useRef(null);
+
+  let tl = new TimelineLite();
+
+  useEffect(() => {
+    // Images vars
+    const girlImage = images.firstElementChild;
+    const boyImage = images.lastElementChild;
+
+
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
+
+    tl.from(girlImage, 1.2, { y: 1280, ease: Power3.easeOut })
+      .from(girlImage.firstElementChild, 2, { scale: 1.6, ease: Power3.easeOut }, .2)
+      .from(boyImage, 1.2, { y: 1280, ease: Power3.easeOut }, .2)
+      .from(boyImage.lastElementChild, 2, { scale: 1.6, ease: Power3.easeOut }, .2)
+  });
+
   return (
-    <div className='hero'>
+    <div className='hero' ref={el => app = el}>
       <div className='container'>
         <div className='hero-inner'>
           <div className='hero-content'>
@@ -44,7 +63,7 @@ function App() {
             </div>
           </div>
           <div className='hero-images'>
-            <div className='hero-images-inner'>
+            <div className='hero-images-inner' ref={el => images = el}>
               <div className='hero-image girl'>
                 <img src={imgGirl} alt='girl' />
               </div>
