74f817a3c496a16d230ff993a2826ed6308985fa
morganfula <morgan.fula@me.com>
2020-09-20 12:41:42 +0200

    starting app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
 import React from "react";
+
+import arrow from "./images/arrow-right.svg";
 import "./App.scss";
 
 function App() {
-  return <div className='hero'></div>;
+  return (
+    <div className='hero'>
+      <div className='container'>
+        <div className='hero-inner'>
+          <div className='hero-content'>
+            <div className='hero-content-inner'>
+              <h1>
+                <div className='hero-content-line'>
+                  <div className='hero-content-line-inner'>
+                    Relieving the burden
+                  </div>
+                </div>
+                <div className='hero-content-line'>
+                  <div className='hero-content-line-inner'>
+                    of disease caused
+                  </div>
+                </div>
+                <div className='hero-content-line'>
+                  <div className='hero-content-line-inner'>by behaviors.</div>
+                </div>
+              </h1>
+              <p>
+                Better treats serious cardiometabolic diseases to transform
+                lives and reduce healthcare utilization through the use of
+                digital therapeutics.
+              </p>
+              <div className='btn-row'>
+                <button className='explore-button'>
+                  Explore
+                  <div className='arrow-icon'>
+                    <img src={arrow} alt='row' />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='hero-images'></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
